784f9e5e7aa9e737f7ac75630788135182bcb383
daniel <necoraio@hotmail.com>
2024-08-15 15:01:11 -0300

    adicionado o create no UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,16 @@
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UserController {
-  create(req, res) {
+  async create(req, res) {
     const { password, name, email } = req.body
-    if (!name) {
-      throw new AppError('name is required')
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    if (checkUserExists) {
+      throw new AppError('User already exists')
     }
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password])
+
     res.status(201).json({ password, name, email })
   }
 }
