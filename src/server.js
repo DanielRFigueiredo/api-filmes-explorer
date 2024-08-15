9337b24098daf096415f7c33ddd97447a6219f08
daniel <necoraio@hotmail.com>
2024-08-15 14:06:06 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
