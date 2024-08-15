50a5ede88c434df842acdc5f9691c0569056a845
daniel <necoraio@hotmail.com>
2024-08-15 14:49:03 -0300

    criado e configurado o AppError.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UserController {
   create(req, res) {
     const { password, name, email } = req.body
-    res.status(201).json({password, name, email})
+    if (!name) {
+      throw new AppError('name is required')
+    }
+    res.status(201).json({ password, name, email })
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+const AppError = require('./utils/AppError');
 const migrations = require('./database/sqlite/migration')
 
 const express = require('express');
@@ -12,6 +13,21 @@ migrations()
 app.use('/', express.json())
 app.use('/', routes)
 
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  console.error(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+
+})
 
 
 
