7c25465a4407314b020082a06592e2bfb55dbfc0
Daniel Figueiredo <necoraio@hotmail.com>
2024-09-04 16:26:51 -0300

    feat: login

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -9,22 +9,21 @@ class SessionsController {
   async create(req, res) {
     const { email, password } = req.body
     const user = await knex('users').where({ email }).first()
-
+    
     if (!user) {
       throw new AppError('Email ou/e Senha Incorretos', 401)
     }
     const checkMatchPassword = await compare(password, user.password)
-
+    
     if (!checkMatchPassword) {
       throw new AppError('Email ou/e Senha Incorretos', 401)
     }
-
+    
     const token = sign({}, jwt.secret, {
       subject: String(user.id),
       expiresIn: jwt.expiresIn
     })
-
-
+    
     res.json({ user, token })
   }
 }
